81f959e1f53e63787d7c27bbf39ca7afbda600ca
amardeepmalale96 <161214586+amardeepmalale96@users.noreply.github.com>
2024-10-07 17:34:26 +0530

    Update ShoppingCartApplication.java

--- src/main/java/com/reljicd/ShoppingCartApplication.java
+++ src/main/java/com/reljicd/ShoppingCartApplication.java
@@ -1,26 +1,21 @@
-package com.example.demo;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class BankingController {
-
-    @GetMapping("/")
-    public String index(Model model) {
-        model.addAttribute("message", "Welcome to the Banking Domain Web Application");
-        return "index";
-    }
-
-    @GetMapping("/banking_fundamentals")
-    public String bankingFundamentals(Model model) {
-        model.addAttribute("fundamentals", getBankingFundamentals());
-        return "banking_fundamentals";
-    }
-
-    // Example method to get banking fundamentals
-    private String getBankingFundamentals() {
-        return "A bank is a financial institution...";
-    }
-}
+target/
+!.mvn/wrapper/maven-wrapper.jar
+### STS ###
+.apt_generated
+.classpath
+.factorypath
+.project
+.settings
+.springBeans
+### IntelliJ IDEA ###
+.idea
+*.iws
+*.iml
+*.ipr
+### NetBeans ###
+nbproject/private/
+build/
+nbbuild/
+dist/
+nbdist/
+.nb-gradle/
